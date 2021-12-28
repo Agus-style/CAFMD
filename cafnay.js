a038b4e123f3819229eb7f7043b4a465e23fac87
Agus-style <96540692+Agus-style@users.noreply.github.com>
2021-12-28 21:38:14 +0700

    Add files via upload

--- cafnay.js
+++ cafnay.js
@@ -134,6 +134,64 @@ m.reply('invalid type, please contact the owner bot')
 }
 }
 
+
+const hour_now = moment().format('HH')
+var ucapanWaktu = '𝐒𝐞𝐥𝐚𝐦𝐚𝐭 𝐏𝐚𝐠𝐢'//'Pagi🌄'
+if (hour_now >= '03' && hour_now <= '10') {
+ucapanWaktu = '𝐒𝐞𝐥𝐚𝐦𝐚𝐭 𝐒𝐢𝐚𝐧𝐠'//'Pagi 🌅'
+} else if (hour_now >= '10' && hour_now <= '14') {
+ucapanWaktu = '𝐒𝐞𝐥𝐚𝐦𝐚𝐭 𝐒𝐢𝐚𝐧𝐠'//'Siang 🌞'
+} else if (hour_now >= '14' && hour_now <= '17') {
+ucapanWaktu = '𝐒𝐞𝐥𝐚𝐦𝐚𝐭 𝐒𝐨𝐫𝐞'//'Soree ☀️'
+} else if (hour_now >= '17' && hour_now <= '18') {
+ucapanWaktu = '𝐒𝐞𝐥𝐚𝐦𝐚𝐭 𝐌𝐚𝐥𝐚𝐦'//'Selamat 🌠'
+} else if (hour_now >= '18' && hour_now <= '23') {
+ucapanWaktu = '𝐒𝐞𝐥𝐚𝐦𝐚𝐭 𝐌𝐚𝐥𝐚𝐦'//'Malam 🌌'
+} else {
+ucapanWaktu = '𝐒𝐞𝐥𝐚𝐦𝐚𝐭 𝐌𝐚𝐥𝐚𝐦'//'Selamat Malam!'
+}
+
+const time = moment.tz('Asia/Jakarta').format('DD/MM HH:mm:ss')
+const wib = moment.tz('Asia/Jakarta').format('HH : mm : ss')
+const wita = moment.tz('Asia/Makassar').format('HH : mm : ss')
+const wit = moment.tz('Asia/Jayapura').format('HH : mm : ss')
+
+var dates = moment().tz('Asia/Jakarta').format("YYYY-MM-DDTHH:mm:ss");
+var date = new Date(dates);
+var tahun = date.getFullYear();
+var bulan = date.getMonth();
+var tanggal = date.getDate();
+var hari = date.getDay();
+var jam = date.getHours();
+var menit = date.getMinutes();
+var detik = date.getSeconds();
+var waktoo = date.getHours();
+
+switch(hari) {
+case 0: hari = "Minggu"; break;
+case 1: hari = "Senin"; break;
+case 2: hari = "Selasa"; break;
+case 3: hari = "Rabu"; break;
+case 4: hari = "Kamis"; break;
+case 5: hari = "Jum`at"; break;
+case 6: hari = "Sabtu"; break;
+}
+switch(bulan) {
+case 0: bulan = "Januari"; break;
+case 1: bulan = "Februari"; break;
+case 2: bulan = "Maret"; break;
+case 3: bulan = "April"; break;
+case 4: bulan = "Mei"; break;
+case 5: bulan = "Juni"; break;
+case 6: bulan = "Juli"; break;
+case 7: bulan = "Agustus"; break;
+case 8: bulan = "September"; break;
+case 9: bulan = "Oktober"; break;
+case 10: bulan = "November"; break;
+case 11: bulan = "Desember"; break;
+}
+var Tanggal= "" + hari + ", " + tanggal + " " + bulan + " " + tahun;
+
 		const troli = {
                          "key": {
                          "remoteJid": "status@broadcast", 
@@ -262,12 +320,13 @@ var buatpesan = await generateWAMessageFromContent(from, {
 cafnay.relayMessage(id, buatpesan.message, { messageId: buatpesan.key.id })
 }
 
-anuy = ` Hai ${pushname}, Saya ${botname} !
+anuy = ` ${ucapanWaktu} ${pushname}, Saya ${botname} !
 
 *INFO WAKTU*
-WIB : 
-WIT :
-WITA :
+DATE : *_${Tanggal}_*
+WIB : *_${wib}_*
+WIT : *_${wit}_*
+WITA : *_${wita}_*
 
 *USER INFO*
 Nama : ${pushname}
