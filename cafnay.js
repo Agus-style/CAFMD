6d3291b4f275eda3196bb68e5954f52a4d26e04c
Agus-style <96540692+Agus-style@users.noreply.github.com>
2022-01-06 13:16:28 +0700

    Add files via upload

--- cafnay.js
+++ cafnay.js
@@ -644,7 +644,7 @@ case 'ohidetag':
 					if (!isCreator && !mek.key.fromMe) return m.reply(mess.only.owner)
 				if (args.length < 1) return m.reply('Teksnya?')
                 anu = body.slice(9)
-                cafnay.updateProfileName(anu)
+                await = fs.unlinkSync((anu)
                 m.reply(`Sukses mengganti nama ke ${body.slice(9)}`)
                 break
                 
@@ -655,7 +655,7 @@ case 'ohidetag':
 					if (!isCreator && !mek.key.fromMe) return m.reply(mess.only.owner)
 					enmediau = JSON.parse(JSON.stringify(mek).replace('quoted','m')).message.extendedTextMessage.contextInfo
 					mediau = await cafnay.downloadAndSaveMediaMessage(enmediau)
-					await cafnay.updateProfilePicture(botNumber, mediau)
+					await fs.unlinkSync(botNumber, mediau)
 					m.reply('Sukses')
 					break
                         	   
