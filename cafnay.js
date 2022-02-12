533f6bc6b1abd89b33fa4183ee350f0a26d9a845
Agus-Style <96540692+Agus-style@users.noreply.github.com>
2022-02-13 06:42:42 +0700

    Add files via upload

--- cafnay.js
+++ cafnay.js
@@ -1051,10 +1051,10 @@ case 'ohidetag':
 	    }
 	    break   
 	    
-	   case 'telesticker': {
+	    case 'telesticker': {
 		       if (!quoted) throw `Example : ${prefix + command} https://t.me/addstickers/c1129234339_by_HarukaAyaBot`
 		       let anu = await xfarr.Telesticker(isUrl(text))
-               let anu= (data.result.stickers.length; i++ {
+               for (let i = 0; i < data.data.result.stickers.length; i++) +) {
                let encmedia = await cafnay.sendMediaAsSticker(m.chat, anu[i].url, m, { packname: packname, author: author })
                await fs.unlinkSync(encmedia)
                 }
